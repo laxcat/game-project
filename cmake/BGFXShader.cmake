b031b72e43875f24bc7cba895b19457fa06bece0
Tyler Martin <tyler@tylermartin.net>
2021-10-06 18:41:24 -0600

    add shader building through cmake
    the bgfx-provided Makefiles weren't quite working for this setup. also this is cleaner, sticks with cmake, etc.

--- cmake/BGFXShader.cmake
+++ cmake/BGFXShader.cmake
@@ -1,18 +1,44 @@
-set(SHADERC build/_deps/bgfx_content-build/shaderc)
-set(INCLUDE_DIR build/_deps/bgfx_content-src/bgfx/src)
-set(OUTPUT_DIR src/shaders/main)
-set(OUTPUT ${OUTPUT_DIR}/vs_main.sc.bin)
+#
+# Loops through subdirectories and looks for BGFX shaders and compiles them.
+#
+function(BGFXShader_compile shader_dir_rel_to_root)
+    # vars
+    set(root_dir ${CMAKE_CURRENT_SOURCE_DIR})
+    set(shaderc ${root_dir}/build/_deps/bgfx_content-build/shaderc)
+    set(include_dir ${root_dir}/build/_deps/bgfx_content-src/bgfx/src)
+    set(shader_dir ${root_dir}/${shader_dir_rel_to_root})
+    set(depends)
 
-macro(BGFXShader_compile)
-add_custom_command(
-    DEPENDS ${SHADERC} ${INCLUDE_DIR}
-    OUTPUT  ${OUTPUT}
-    COMMAND ${SHADERC}
-            --platform osx
-            -p metal
-            --type f
-            -f ${OUTPUT_DIR}/vs_main.sc
-            -o ${OUTPUT}
-            -i ${INCLUDE_DIR}
-)
-endmacro()
+    # run for each individual shader
+    macro(compile dir type)
+        set(output_dir ${shader_dir}/${dir})
+        set(output ${output_dir}/${type}s_${dir}.sc.bin)
+        set(input ${output_dir}/${type}s_${dir}.sc)
+
+        if(EXISTS ${input})
+            add_custom_command(
+                DEPENDS ${shaderc} ${include_dir} ${input}
+                OUTPUT  ${output}
+                COMMAND ${shaderc}
+                ARGS    --platform osx -p metal --type ${type} -f ${input} -o ${output} -i ${include_dir}
+            )
+            list(APPEND depends ${output})
+        endif()
+    endmacro()
+    
+    # loop through subdirectories
+    file(GLOB children RELATIVE "${shader_dir}" "${shader_dir}/*")
+    foreach(child ${children})
+        if(IS_DIRECTORY ${shader_dir}/${child})
+            compile(${child} v)
+            compile(${child} f)
+            compile(${child} c)
+        endif()
+    endforeach()
+
+    # gathers all output files as dependents of blank target
+    add_custom_target(
+      BGFXShader_target
+      DEPENDS ${depends}
+    )
+endfunction()
