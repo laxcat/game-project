3834d4f6136caffa270dcb6bb9df10711ebbea9d
Tyler Martin <tyler@tylermartin.net>
2022-08-24 17:11:07 -0600

    update pool claim functions

--- engine/common/MemSys.h
+++ engine/common/MemSys.h
@@ -92,21 +92,51 @@ public:
         size_t freeIndex() const { return _freeIndex; }
         byte_t * data() const { return (byte_t *)this + sizeof(Pool); }
 
-        template <typename T>
-        T * claimTo(size_t index) {
-            assert(sizeof(T) == _objSize);
-            if (index >= _objMaxCount) return nullptr;
-            _freeIndex = index;
-            return &((T *)data())[_freeIndex];
+        // claim up to (but not including) index from current head and return array
+        // do a placement new in each newly claimed slot
+        template <typename T, typename ... TP>
+        T * claimTo(size_t index, TP && ... params) {
+            // checks
+            if (sizeof(T) != _objSize) {
+                fprintf(stderr, "Could not claim, size of T (%zu) does not match _objSize (%zu).",
+                    sizeof(T), _objSize);
+                assert(false);
+            }
+            if (index >= _objMaxCount) {
+                fprintf(stderr, "Could not claim, index (%zu) out of range (max: %zu).",
+                    index, _objMaxCount);
+                return nullptr;
+            }
+            if (index <= _freeIndex) {
+                fprintf(stderr, "Could not claim, freeIndex (%zu) already greater than requested index (%zu).",
+                    _freeIndex, index);
+                return nullptr;
+            }
+
+            // return start of array, at current head
+            T * ptr = at<T>(_freeIndex);
+
+            // do a placement new for all newly claimed
+            // move head to new index
+            for (; _freeIndex < index; ++_freeIndex) {
+                new (at<T>()) T{static_cast<TP&&>(params)...};
+            }
+            return ptr;
+
+        }
+
+        // claim count number of objects and return start of array
+        template <typename T, typename ... TP>
+        T * claim(size_t count = 1, TP && ... params) {
+            return claimTo<T>(_freeIndex + count, static_cast<TP&&>(params)...);
         }
 
         template <typename T>
-        T * claim(size_t count) {
-            assert(sizeof(T) == _objSize);
-            if (_freeIndex + count >= _objMaxCount) return nullptr;
-            _freeIndex += count;
-            return &((T *)data())[_freeIndex];
+        T * at(size_t index) {
+            assert(index < _objMaxCount && "Index out of range.");
+            return (T *)(data() + _objSize * index);
         }
+        template <typename T> T * at() { return at<T>(_freeIndex); }
 
         void reset() { _freeIndex = 0; }
 
