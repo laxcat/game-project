0e914397cfe2fb525d3b1cfdf655891ec53cdbe7
Tyler Martin <tyler@tylermartin.net>
2022-09-08 10:35:33 -0600

    add string utils
    add string utilities for image data and base64 decoding

--- engine/common/string_utils.cpp
+++ engine/common/string_utils.cpp
@@ -1,7 +1,9 @@
 #include "string_utils.h"
 #include <stdio.h>
 #include <filesystem>
+#include <stb_image.h>
 #include "../MrManager.h"
+#include "modp_b64.h"
 
 char const * relPath(char const * path) {
     auto rel = std::filesystem::relative(path);
@@ -15,4 +17,87 @@ void setName(char * dest, char const * name, int size) {
 
 bool strEqu(char const * strA, char const * strB) {
     return (strcmp(strA, strB) == 0);
+}
+
+bool strEqualForLength(char const * strA, char const * strB, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        // one (but not both) is null or chars don't match
+        if (!strA[i] != !strB[i] || strA[i] != strB[i]) return false;
+    }
+    return true;
+}
+
+bool strWithin(char const * str, char const * strGroup, char sep) {
+    assert(str && strGroup);
+
+    // incrementing pointers
+    char * sp = (char *)str;
+    char * gp = (char *)strGroup;
+
+    // while there are characters in the group pointer
+    // for each "substr" in group
+    while(*gp) {
+        // special check for empty string
+        if (!*sp && *gp == sep) return true;
+
+        // iterate through this "substr"
+        while(*sp) {
+            if (*sp != *gp) break;
+            ++sp, ++gp;
+            if (!*sp && (!*gp || *gp == sep)) return true;
+        }
+
+        // move to next substr
+        while(*gp && *gp != sep) ++gp;
+        if (*gp == sep) ++gp;
+        sp = (char *)str;
+
+        // special check for empty string at end of group
+        if (!*sp && !*gp) return true;
+    }
+    return false;
+}
+
+// SOME STRING TESTS
+// printl("test %d", strWithin("hello", "hello,goodbye"));
+// printl("test %d", strWithin("goodbye", "hello,goodbye"));
+// printl("test %d", strWithin("b", "a,b,c"));
+// printl("test %d", strWithin("", ",b,c"));
+// printl("test %d", strWithin("", "a,,c"));
+// printl("test %d", strWithin("", "a,b,"));
+// printl("test %d", strWithin("hell", "hello,goodbye"));
+
+ImageData loadImageBase64(char const * data, size_t length) {
+    if (strEqualForLength("data:image/jpeg;base64,", data, 23)) {
+        data   += 23;
+        length -= 23;
+    }
+    if (strEqualForLength("data:image/png;base64,", data, 22)) {
+        data   += 22;
+        length -= 22;
+    }
+
+    size_t decodedLength = modp_b64_decode_len(length);
+    // printl("length %zu", length);
+    // printl("modp_b64_decode_len %zu", decodedLength);
+
+    byte_t * decodedData = (byte_t *)malloc(decodedLength);
+    modp_b64_decode((char *)decodedData, data, length);
+
+    ImageData id;
+    byte_t * imgData = stbi_load_from_memory(decodedData, decodedLength, &id.width, &id.height, &id.nChannels, 0);
+    if (!imgData) return id;
+
+    id.data = mm.frameStack->alloc(id.dataSize());
+    if (!id.data) {
+        fprintf(stderr, "Frame stack alloc failed.\n");
+        id = {};
+        stbi_image_free(imgData);
+        return id;
+    }
+    memcpy(id.data, imgData, id.dataSize());
+
+    stbi_image_free(imgData);
+
+    return id;
 }
--- engine/common/string_utils.h
+++ engine/common/string_utils.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <stddef.h>
+#include "types.h"
 
 char const * relPath(char const * path);
+
 void setName(char * dest, char const * name, int size = 32);
+
 bool strEqu(char const * strA, char const * strB);
+
+bool strEqualForLength(char const * strA, char const * strB, size_t length);
+
+// finds if str exactly matches a sub string in a comma seperated strGroup
+// eg:
+// strWithin("hello", "hello,goodbye") // return true
+// strWithin("hell", "hello,goodbye") // return false
+bool strWithin(char const * str, char const * strGroup, char sep = ',');
+
+struct ImageData {
+    byte_t * data;
+    int width = 0;
+    int height = 0;
+    int nChannels = 0;
+    size_t dataSize() const { return width * height * nChannels; };
+};
+ImageData loadImageBase64(char const * data, size_t length);
