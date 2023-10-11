7a723abb54d104e5cc7028fa442ab8ade25954f1
Tyler Martin <tyler@tylermartin.net>
2023-10-10 18:15:20 -0600

    update print16f indent

--- engine/dev/print.h
+++ engine/dev/print.h
@@ -71,19 +71,19 @@ inline void print4f(float const * f) {
     );
 }
 
-inline void print16f(float const * f, char const * indent) {
+inline void print16f(float const * f, uint16_t indent = 0) {
     print(
-        "%s%0.4f %0.4f %0.4f %0.4f\n"
-        "%s%0.4f %0.4f %0.4f %0.4f\n"
-        "%s%0.4f %0.4f %0.4f %0.4f\n"
-        "%s%0.4f %0.4f %0.4f %0.4f\n", 
-        indent, f[ 0], f[ 1], f[ 2], f[ 3],
-        indent, f[ 4], f[ 5], f[ 6], f[ 7],
-        indent, f[ 8], f[ 9], f[10], f[11],
-        indent, f[12], f[13], f[14], f[15]
+        "%*c%0.4f %0.4f %0.4f %0.4f\n"
+        "%*c%0.4f %0.4f %0.4f %0.4f\n"
+        "%*c%0.4f %0.4f %0.4f %0.4f\n"
+        "%*c%0.4f %0.4f %0.4f %0.4f\n",
+        indent, ' ', f[ 0], f[ 1], f[ 2], f[ 3],
+        indent, ' ', f[ 4], f[ 5], f[ 6], f[ 7],
+        indent, ' ', f[ 8], f[ 9], f[10], f[11],
+        indent, ' ', f[12], f[13], f[14], f[15]
     );
 }
-inline void print16fc(bool shouldPrint, float const * f, char const * indent) {
+inline void print16fc(bool shouldPrint, float const * f,uint16_t indent = 0) {
 
     if (shouldPrint) print16f(f, indent);
 }
